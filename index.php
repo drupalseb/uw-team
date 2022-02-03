f222431e436413f7bd3337ef4cbe0d447bb273ca
drupalseb <drupalseb@gmail.com>
2022-02-03 19:50:06 +0100

    added jquery CDN

--- index.php
+++ index.php
@@ -7,6 +7,10 @@ error_reporting(E_ALL);
 <head>
   <meta charset="utf-8">
   <title>Lista zakupów</title>
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"
+            integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+            crossorigin="anonymous">
+  </script>
 </head>
 <body>
 <?php
